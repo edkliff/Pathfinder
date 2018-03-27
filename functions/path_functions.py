151fdbf22a89424d3c21747bdd64e2632ce8338f
edkliff <1qaZXsw2>
2018-03-27 17:49:07 +0300

    Documentation

--- functions/path_functions.py
+++ functions/path_functions.py
@@ -1,18 +1,29 @@
 import json
 
-
+# path graf init
 with open('data/paths.json') as json_data:
-    graf = json.load(json_data)
+    path_graf = json.load(json_data)
 
+# way types init
 with open('data/way_types.json') as json_data:
     way_types = json.load(json_data)
 
 
+# breadth-first search in path graf
 def find_all_pathes(start, finish, path_index, good_pathes, pathes):
+    """
+    recursive function for finding all paths between two points
+    :param start: the index of the start point(from path_graf)
+    :param finish: the index of the finish point(from path_graf)
+    :param path_index: the index of the current path
+    :param good_pathes: a list of paths ending at the destination point
+    :param pathes: list of all paths
+    :return: returns nothing, fills the variable good_pathes from outer space
+    """
     path = list(pathes[path_index])
     path.append(start)
     pathes.append(path)
-    for next_point in graf[start]['links']:
+    for next_point in path_graf[start]['links']:
         count = 0
         if next_point in path:
             pass
@@ -31,13 +42,26 @@ def find_all_pathes(start, finish, path_index, good_pathes, pathes):
 
 
 def pathfinder(start_point, finish_point):
-    gp = []
-    p = [[start_point], ]
+    """
+    the function initializes the list of all paths between points and fills this list
+    :param start_point: the index of the start point(from path_graf)
+    :param finish_point: the index of the finish point(from path_graf)
+    :return: list of all pathes
+    """
+    gp = []  # variable for fill
+    p = [[start_point], ]  # Bad, very bad. Only for "not empty" list
     find_all_pathes(start_point, finish_point, 0, gp, p)
     return gp
 
 
 def list_minus(inner_data, i1, i2):
+    """
+    comparing lists with path vertices
+    :param inner_data: list of paths
+    :param i1: first path
+    :param i2: second path
+    :return: the length of the set of elements contained in the first path, but not contained in the second
+    """
     return len(set(inner_data[i1]) - set(inner_data[i2]))
 
 
@@ -60,14 +84,14 @@ def calc_time(dataset):
     for line in dataset:
         time = 0
         for i in range(0, len(line) - 1):
-            time += int(graf[line[i]]['links'][line[i+1]])
+            time += int(path_graf[line[i]]['links'][line[i + 1]])
             way_type = calc_way_type(line[i], line[i+1])
             # if way_type == 0:
 
             print('{} from {} to {}, {} minutes'.format(way_types[str(way_type)],
-                                                        graf[line[i]]['name'],
-                                                        graf[line[i+1]]['name'],
-                                                        graf[line[i]]['links'][line[i+1]]))
+                                                        path_graf[line[i]]['name'],
+                                                        path_graf[line[i + 1]]['name'],
+                                                        path_graf[line[i]]['links'][line[i + 1]]))
         print(time, 'min')
         print('========================')
 
@@ -82,8 +106,8 @@ def calc_way_type(point1, point2):
     2 - change
     3 - walk
     """
-    start_type = graf[point1]['type']
-    finish_type = graf[point2]['type']
+    start_type = path_graf[point1]['type']
+    finish_type = path_graf[point2]['type']
     if start_type == 'walk' or finish_type == 'walk':
         return 3
     elif start_type == finish_type and start_type == 'subtrain':
