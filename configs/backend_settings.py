efa69531a266dc6a3b5d5c6f0132f79a988dc7a8
edkliff <1qaZXsw2>
2018-03-28 11:34:14 +0300

    Venv

--- configs/backend_settings.py
+++ configs/backend_settings.py
@@ -3,4 +3,4 @@ FINISH_POINT = '12'         # finish point for debug
 TIME_FOR_RUN_UP = 10        # minutes before start way
 MIN_SUBTRAIN_WAITING = 7    # minutes for subtrain waiting
 
-yandex_api_key = 'insert yandex key'
+yandex_api_key = 'insert key'
